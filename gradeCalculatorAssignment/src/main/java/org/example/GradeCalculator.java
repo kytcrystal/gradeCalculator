4deafdd121f6871d765f456734bffc1e66d20f1a
Crystal Kwok <kwokyatingcrystal@gmail.com>
2023-10-21 11:54:02 +0200

    Eighth TDD Cycle

--- gradeCalculatorAssignment/src/main/java/org/example/GradeCalculator.java
+++ gradeCalculatorAssignment/src/main/java/org/example/GradeCalculator.java
@@ -1,5 +1,8 @@
 package org.example;
 
+import java.util.Dictionary;
+import java.util.Hashtable;
+
 public class GradeCalculator {
 
     public static final int FULL_LAB_POINTS = 50;
@@ -11,34 +14,50 @@ public class GradeCalculator {
 
     public static Grade computeGrade(int labPoints, float examGrade) {
         Grade grade = new Grade();
-        int extraExamGrade = 0;
-        int unusedLabPoints = 0;
-        if (labPoints > FULL_LAB_POINTS) {
-            int extraLabPoints = labPoints - FULL_LAB_POINTS;
-            extraExamGrade = (int) Math.floor((double) extraLabPoints / 5);
-            if (extraExamGrade>3) {
-                extraExamGrade = 3;
-            }
-            labPoints = FULL_LAB_POINTS;
-            unusedLabPoints = extraLabPoints - extraExamGrade*5;
-        }
-        float finalLabPoints = (float) (labPoints * MAX_TOTAL_GRADE/2) / FULL_LAB_POINTS;
+        Dictionary<String, Integer> labPointsDict = computeLabPoints(labPoints);
+        float finalLabPoints = (float) (labPointsDict.get("ComputedLabPoints") * MAX_TOTAL_GRADE/2) / FULL_LAB_POINTS;
         float finalExamGrade = examGrade * (MAX_TOTAL_GRADE/2) / FULL_EXAM_GRADE;
-        float finalGrade = finalLabPoints + finalExamGrade + extraExamGrade;
+        float finalGrade = finalLabPoints + finalExamGrade + labPointsDict.get("ExtraExamGrade");
         determineApprovedGrade(finalGrade, grade);
-        determineNumericGrade(finalGrade, grade);
-        determineCumLaude(finalGrade, grade,unusedLabPoints);
+        determineNumericGrade(finalGrade, grade, labPointsDict.get("ComputedLabPoints"));
+        determineCumLaude(finalGrade, grade,labPointsDict.get("UnusedLabPoints"));
         return grade;
     }
 
+    public static Dictionary<String, Integer> computeLabPoints(int labPoints) {
+        Dictionary<String, Integer> labPointsDictionary = new Hashtable<>();
+        labPointsDictionary.put("ExtraExamGrade", 0);
+        labPointsDictionary.put("UnusedLabPoints", 0);
+        labPointsDictionary.put("ComputedLabPoints", labPoints);
+        if (labPoints > FULL_LAB_POINTS) {
+            int extraLabPoints = labPoints - FULL_LAB_POINTS;
+            int extraExamGrade = (int) Math.floor((double) extraLabPoints / 5);
+            if (extraExamGrade>3) {
+                extraExamGrade = 3;
+            }
+            labPointsDictionary.put("ExtraExamGrade", extraExamGrade);
+            labPointsDictionary.put("ComputedLabPoints", FULL_LAB_POINTS);
+            int unusedLabPoints = extraLabPoints - extraExamGrade * 5;
+            labPointsDictionary.put("UnusedLabPoints", unusedLabPoints);
+        }
+        return labPointsDictionary;
+    }
 
-    private static void determineNumericGrade(float finalGrade, Grade grade) {
+
+    private static void determineNumericGrade(float finalGrade, Grade grade, int labPoints) {
         if (finalGrade<MIN_APPROVED_GRADE) {
             grade.setNumericGrade(null);
-        } else if (finalGrade>MAX_TOTAL_GRADE) {
+        } else if (finalGrade>=MAX_TOTAL_GRADE) {
             grade.setNumericGrade(MAX_TOTAL_GRADE);
         } else {
-            grade.setNumericGrade((int) finalGrade);
+            if (labPoints<25) {
+                grade.setNumericGrade((int) finalGrade);
+            } else if (labPoints>40) {
+                grade.setNumericGrade(finalGrade%1>0 ? (int) finalGrade + 1 : (int) finalGrade);
+            }
+            else {
+                grade.setNumericGrade(Math.round(finalGrade));
+            }
         }
     }
 
--- gradeCalculatorAssignment/src/test/java/org/example/GradeCalculatorTest.java
+++ gradeCalculatorAssignment/src/test/java/org/example/GradeCalculatorTest.java
@@ -64,6 +64,51 @@ class GradeCalculatorTest {
     }
 
     @Test
+    void gradeRoundedDownTest() {
+        Grade grade = GradeCalculator.computeGrade(23,10.0F);
+        //finalLabPoints = 6.9, finalGrade = 21.9
+        assertTrue(grade.getApproved());
+        assertEquals(21,grade.getNumericGrade());
+        assertFalse(grade.getCumLaude());
+    }
+
+    @Test
+    void gradeRoundedNormallyUpTest() {
+        Grade grade = GradeCalculator.computeGrade(26,10.0F);
+        //finalLabPoints = 7.8, finalGrade = 22.8
+        assertTrue(grade.getApproved());
+        assertEquals(23,grade.getNumericGrade());
+        assertFalse(grade.getCumLaude());
+    }
+
+    @Test
+    void gradeRoundedNormallyDownTest() {
+        Grade grade = GradeCalculator.computeGrade(27,10.0F);
+        //finalLabPoints = 8.1, finalGrade = 23.1
+        assertTrue(grade.getApproved());
+        assertEquals(23,grade.getNumericGrade());
+        assertFalse(grade.getCumLaude());
+    }
+
+    @Test
+    void gradeRoundedUpTest() {
+        Grade grade = GradeCalculator.computeGrade(41,10.0F);
+        //finalLabPoints = 12.3, finalGrade = 27.3
+        assertTrue(grade.getApproved());
+        assertEquals(28,grade.getNumericGrade());
+        assertFalse(grade.getCumLaude());
+    }
+
+    @Test
+    @Disabled
+    void exceedingLabPointsAboveMaximumWithCumLaudeTest() {
+        Grade grade = GradeCalculator.computeGrade(66,8.0F);
+        assertTrue(grade.getApproved());
+        assertEquals(30,grade.getNumericGrade());
+        assertTrue(grade.getCumLaude());
+    }
+
+    @Test
     @Disabled
     void exceedingLabPointWithoutCumLaudeTest() {
         Grade grade = GradeCalculator.computeGrade(51,8.0F);
