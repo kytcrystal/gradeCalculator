9b79b023cde45fdc938128f87af674a582d70daf
Crystal Kwok <kwokyatingcrystal@gmail.com>
2023-10-21 10:08:11 +0200

    Fifth TDD Cycle Corrected

--- gradeCalculatorAssignment/src/main/java/org/example/GradeCalculator.java
+++ gradeCalculatorAssignment/src/main/java/org/example/GradeCalculator.java
@@ -2,44 +2,47 @@ package org.example;
 
 public class GradeCalculator {
 
+    public static final int FULL_LAB_POINTS = 50;
+    public static final int FULL_EXAM_GRADE = 10;
+    public static final int MAX_TOTAL_GRADE = 30;
+    public static final int MIN_APPROVED_GRADE = 18;
+
+
+
     public static Grade computeGrade(int labPoints, float examGrade) {
         Grade grade = new Grade();
         int extraExamGrade = 0;
-        float finalLabPoints = computeLabPoints(labPoints, extraExamGrade);
-        float finalExamGrade = examGrade * 15 / 10;
-        float finalGrade = finalLabPoints + finalExamGrade + extraExamGrade;
+        if (labPoints > FULL_LAB_POINTS) {
+            int extraLabPoints = labPoints - FULL_LAB_POINTS;
+            extraExamGrade = (int) Math.floor((double) extraLabPoints / 5);
+            labPoints = FULL_LAB_POINTS;
+        }
+        float finalLabPoints = (float) (labPoints * MAX_TOTAL_GRADE/2) / FULL_LAB_POINTS;
+        float finalExamGrade = examGrade * (MAX_TOTAL_GRADE/2) / FULL_EXAM_GRADE;
+        float finalGrade = (float) finalLabPoints + finalExamGrade + (int) extraExamGrade;
         determineApprovedGrade(finalGrade, grade);
         determineNumericGrade(finalGrade, grade);
         determineCumLaude(finalGrade, grade);
         return grade;
     }
 
-    public static float computeLabPoints(int labPoints, int extraExamGrade) {
-        if (labPoints > 50) {
-            int extraLabPoints = labPoints - 50;
-            extraExamGrade = (int) Math.floor((double) extraLabPoints / 5);
-            labPoints = 50;
-        }
-        return (float) (labPoints * 15) / 50;
-    }
-
 
     private static void determineNumericGrade(float finalGrade, Grade grade) {
-        if (finalGrade<18) {
+        if (finalGrade<MIN_APPROVED_GRADE) {
             grade.setNumericGrade(null);
-        } else if (finalGrade>30) {
-            grade.setNumericGrade(30);
+        } else if (finalGrade>MAX_TOTAL_GRADE) {
+            grade.setNumericGrade(MAX_TOTAL_GRADE);
         } else {
             grade.setNumericGrade((int) finalGrade);
         }
     }
 
     private static void determineApprovedGrade(float finalGrade, Grade grade) {
-        grade.setApproved(finalGrade>=18);
+        grade.setApproved(finalGrade>=MIN_APPROVED_GRADE);
     }
 
     private static void determineCumLaude(float finalGrade, Grade grade) {
-        grade.setCumLaude(finalGrade > 30);
+        grade.setCumLaude(finalGrade > MAX_TOTAL_GRADE);
     }
 
 
